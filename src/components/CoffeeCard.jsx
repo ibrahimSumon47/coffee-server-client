704093b6d2db92637b69903518d026a90092cf67
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-05-09 22:51:41 +0600

    when i click X button coffee got deleted

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,40 @@
+import { data } from "autoprefixer";
 import React from "react";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, category, details, img } = coffee;
+  const { _id, name, quantity, supplier, taste, category, details, img } =
+    coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+            method: "DELETE"
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+                Swal.fire(
+                  'Deleted!',
+                  'Your coffee has been deleted.',
+                  'success'
+                )
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div className="card card-side bg-base-100 shadow-xl">
@@ -16,10 +49,15 @@ const CoffeeCard = ({ coffee }) => {
           <p>{taste}</p>
         </div>
         <div className="card-actions justify-end">
-          <div className="btn-group btn-group-vertical">
-            <button className="btn btn-active">Button</button>
-            <button className="btn">Button</button>
-            <button className="btn">Button</button>
+          <div className="btn-group btn-group-vertical space-y-4">
+            <button className="btn">View</button>
+            <button className="btn">Edit</button>
+            <button
+              className="btn bg-orange-500"
+              onClick={() => handleDelete(_id)}
+            >
+              X
+            </button>
           </div>
         </div>
       </div>
